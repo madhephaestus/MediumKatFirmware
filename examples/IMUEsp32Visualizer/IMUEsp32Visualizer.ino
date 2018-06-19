564e08f724242b3100feb2505a80ce8a6956fe00
Kevin Harrington <mad.hephaestus@gmail.com>
2018-06-19 13:04:13 -0400

    Adding IMU Visualizer

---
+++ examples/IMUEsp32Visualizer/IMUEsp32Visualizer.ino
@@ -0,0 +1,32 @@
+#include <SimplePacketComs.h>
+#include <Wire.h>
+#include <Adafruit_BNO055.h>
+#include <MediumKatFirmware.h>
+#if defined(ARDUINO_ARCH_ESP32)
+#include <Esp32SimplePacketComs.h>
+#include <wifi/WifiManager.h>
+UDPSimplePacket coms;
+WifiManager manager;
+#else
+#include <Teensy32SimplePacketComs.h>
+HIDSimplePacket coms;
+#endif
+
+GetIMU * sensor;
+void setup() {
+#if defined(ARDUINO_ARCH_ESP32)
+	manager.setup();
+#endif
+	sensor = new GetIMU();
+	sensor->startSensor();
+	coms.attach((PacketEventAbstract *) sensor);
+}
+
+void loop() {
+#if defined(ARDUINO_ARCH_ESP32)
+	manager.loop();
+	if(manager.getState()==Connected)
+#endif
+	coms.server();
+	sensor->loop();
+}
--- src/SetServos.h
+++ src/SetServos.h
@@ -9,13 +9,21 @@
 #define LIBRARIES_MEDIUMKATFIRMWARE_SRC_SETSERVOS_H_
 #include <SimplePacketComs.h>
 #include "Arduino.h"
+#if defined(ARDUINO_ARCH_ESP32)
+#include <ESP32Servo.h>
+#else
 #include <PWMServo.h>
+#endif
+
 class SetServos: public PacketEventAbstract {
 private:
 	unsigned long long lastTime = 0;
 
-
+#if defined(ARDUINO_ARCH_ESP32)
+	Servo servos[20];
+#else
 	PWMServo servos[20];
+#endif
 	int currentPos[20] = {90,90,90,90,90,90,90,90,90,90,90,90,90,90,90,90,90,90,90,90};
 	int ServoPins[20] = { 10, 30, 29, 21, 22, 6, 8, 4, 38, 37, 35, 5, 2, 14, 36, 9,
 			3, 7, 23, 20 };	//{9,8, 4, 5, 2,3, 7, 6, 22, 21, 10, 29, 30, 23, 20, 14, 36, 35, 37, 38}
