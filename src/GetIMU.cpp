9136bf6daaa8eec43c05168337a8acdcdd379f7d
Kevin Harrington <mad.hephaestus@gmail.com>
2018-06-19 12:48:22 -0400

    Adding files

---
+++ src/GetIMU.cpp
@@ -0,0 +1,35 @@
+/*
+ * GetIMU.cpp
+ *
+ *  Created on: Jun 7, 2018
+ *      Author: hephaestus
+ */
+
+#include "GetIMU.h"
+
+//User function to be called when a packet comes in
+// Buffer contains data from the packet cming in at the start of the function
+// User data is written into the buffer to send it back
+void GetIMU::event(float * buffer) {
+	if(!started)
+		return;
+	buffer[0] = a.x();
+	buffer[1] = a.y();
+	buffer[2] = a.z();
+	buffer[3] = v.x();
+	buffer[4] = v.y();
+	buffer[5] = v.z();
+}
+void GetIMU::loop() {
+	if(!started)
+		return;
+	a = bno->getVector(Adafruit_BNO055::VECTOR_LINEARACCEL);
+	v = bno->getVector(Adafruit_BNO055::VECTOR_GYROSCOPE);
+
+}
+void GetIMU::startSensor(){
+	bno = new Adafruit_BNO055();
+	started=bno->begin();
+	if(started)
+		bno->setExtCrystalUse(true);
+}
---
+++ src/GetIMU.h
@@ -0,0 +1,39 @@
+/*
+ * GetIMU.h
+ *
+ *  Created on: Jun 7, 2018
+ *      Author: hephaestus
+ */
+
+#ifndef LIBRARIES_MEDIUMKATFIRMWARE_SRC_GETIMU_H_
+#define LIBRARIES_MEDIUMKATFIRMWARE_SRC_GETIMU_H_
+
+#define BNO055_SAMPLERATE_DELAY_MS (1)
+#include <SimplePacketComs.h>
+
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BNO055.h>
+#include <utility/imumaths.h>
+class GetIMU: public PacketEventAbstract {
+private:
+	Adafruit_BNO055 * bno;
+	imu::Vector<3> v;
+	imu::Vector<3> a;
+	bool started;
+public:
+	// Packet ID needs to be set
+	GetIMU() :
+		PacketEventAbstract(1804)	// Address of this event
+{
+		started=false;
+}
+	//User function to be called when a packet comes in
+	// Buffer contains data from the packet cming in at the start of the function
+	// User data is written into the buffer to send it back
+	void event(float * buffer);
+	void loop();
+	void startSensor();
+};
+
+
+#endif /* LIBRARIES_MEDIUMKATFIRMWARE_SRC_GETIMU_H_ */
